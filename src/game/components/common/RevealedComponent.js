4e69bbf4bc780484b1a1cecadfd7dd9c3f65575d
nroutasuo <noora.routasuo@gmail.com>
2018-11-26 22:45:10 +0100

    [save] decrease save size considerably by optimizing Revealed/Visited components

--- src/game/components/common/RevealedComponent.js
+++ src/game/components/common/RevealedComponent.js
@@ -1,12 +1,25 @@
 // Marks the given entity (sector / level) as having been seen by the player or otherwise revealed for the map.
 define(['ash'], function (Ash) {
     var RevealedComponent = Ash.Class.extend({
-        
+
         revealed: false,
-        
+
         constructor: function () {
             this.revealed = true;
+        },
+
+        getSaveKey: function () {
+            return "ScRv";
+        },
+
+        getCustomSaveObject: function () {
+            return {};
+        },
+
+        customLoadFromSave: function (componentValues) {
+            this.revealed = true;
         }
+
     });
 
     return RevealedComponent;
--- src/game/components/common/VisitedComponent.js
+++ src/game/components/common/VisitedComponent.js
@@ -1,10 +1,22 @@
 // Marks the given entity (sector / level) as having been visited by the player.
 define(['ash'], function (Ash) {
     var VisitedComponent = Ash.Class.extend({
-        
+
         visited: false,
-        
+
         constructor: function () {
+            this.visited = true;
+        },
+
+        getSaveKey: function () {
+            return "ScVs";
+        },
+
+        getCustomSaveObject: function () {
+            return {};
+        },
+
+        customLoadFromSave: function (componentValues) {
             this.visited = true;
         }
     });
