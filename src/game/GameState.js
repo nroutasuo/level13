bf6f7cfee0538d6ed200097cd370542d100587f0
nroutasuo <noora.routasuo@gmail.com>
2018-11-27 00:01:48 +0100

    [save] clean up save by removing old action cooldowns

--- src/game/GameState.js
+++ src/game/GameState.js
@@ -1,10 +1,10 @@
 define(['ash', 'game/worldcreator/WorldCreatorHelper'], function (Ash, WorldCreatorHelper) {
     var GameState = Ash.Class.extend({
-        
+
         constructor: function () {
             this.reset();
         },
-        
+
         reset: function () {
             this.level = 0;
             this.worldSeed = 0;
@@ -15,7 +15,7 @@ define(['ash', 'game/worldcreator/WorldCreatorHelper'], function (Ash, WorldCrea
             this.numTradePostCamps = 0;
             this.numVisitedSectors = 0;
             this.isFinished = false;
-            
+
             this.unlockedFeatures = {
                 scavenge: false,
                 scout: false,
@@ -46,7 +46,7 @@ define(['ash', 'game/worldcreator/WorldCreatorHelper'], function (Ash, WorldCrea
                 evidence: false,
                 currency: false,
             };
-            
+
             this.uiStatus = {
                 currentTab: null,
                 mapVisited: false,
@@ -54,68 +54,69 @@ define(['ash', 'game/worldcreator/WorldCreatorHelper'], function (Ash, WorldCrea
                 leaveCampRes: {},
                 leaveCampItems: {},
             };
-            
+
             this.uiBagStatus = {
                 itemsOwnedSeen: [],
                 itemsCraftableUnlockedSeen: [],
                 itemsCraftableAvailableSeen: []
             },
-                
+
             this.foundTradingPartners = []; // camp ordinals
-            
+
             this.actionCooldownEndTimestamps = {};
             this.actionDurationEndTimestamps = {};
         },
-        
+
         passTime: function (seconds) {
-            var cooldownkeys = Object.keys(this.actionCooldownEndTimestamps);                
+            var cooldownkeys = Object.keys(this.actionCooldownEndTimestamps);
             for (var i = 0; i < cooldownkeys.length; i++) {
                 this.actionCooldownEndTimestamps[cooldownkeys[i]] = this.actionCooldownEndTimestamps[cooldownkeys[i]] - seconds * 1000;
             }
         },
-        
+
         getLevelOrdinal: function (level) {
             return WorldCreatorHelper.getLevelOrdinal(this.worldSeed, level);
         },
-        
+
         getLevelForOrdinal: function (levelOrdinal) {
             return WorldCreatorHelper.getLevelForOrdinal(this.worldSeed, levelOrdinal);
         },
-        
+
         getCampOrdinal: function (level) {
             return WorldCreatorHelper.getCampOrdinal(this.worldSeed, level);
         },
-        
+
         getLevelOrdinalForCampOrdinal: function (campOrdinal) {
             return WorldCreatorHelper.getLevelOrdinalForCampOrdinal(this.worldSeed, campOrdinal);
         },
-        
+
         getTotalLevels: function () {
             return WorldCreatorHelper.getHighestLevel(this.worldSeed) - WorldCreatorHelper.getBottomLevel(this.worldSeed) + 1;
         },
-        
+
         getGroundLevel: function () {
             return WorldCreatorHelper.getBottomLevel(this.worldSeed);
         },
-        
+
         getGroundLevelOrdinal: function () {
             return WorldCreatorHelper.getLevelOrdinal(this.worldSeed, WorldCreatorHelper.getBottomLevel(this.worldSeed));
         },
-        
+
         getSurfaceLevel: function () {
             return WorldCreatorHelper.getHighestLevel(this.worldSeed);
         },
-        
+
         getSurfaceLevelOrdinal: function () {
             return WorldCreatorHelper.getLevelOrdinal(this.worldSeed, WorldCreatorHelper.getHighestLevel(this.worldSeed));
         },
-        
+
         setActionCooldown: function (action, key, cooldown) {
+            this.pruneActionCooldowns();
             var actionKey = action;
             if (key.length > 0) actionKey += "-" + key;
             this.actionCooldownEndTimestamps[actionKey] = new Date().getTime() + cooldown * 1000;
         },
-        
+
         getActionCooldown: function (action, key) {
             var actionKey = action;
             if (key.length > 0) actionKey += "-" + key;
@@ -127,13 +128,24 @@ define(['ash', 'game/worldcreator/WorldCreatorHelper'], function (Ash, WorldCrea
             }
             return 0;
         },
-        
+
+        pruneActionCooldowns: function () {
+            var now = new Date().getTime();
+            var cooldownkeys = Object.keys(this.actionCooldownEndTimestamps);
+            for (var i = 0; i < cooldownkeys.length; i++) {
+                var key = cooldownkeys[i];
+                var timestamp = this.actionCooldownEndTimestamps[key];
+                var diff = timestamp - now;
+                if (diff < -30) delete this.actionCooldownEndTimestamps[key];
+            }
+        },
+
         setActionDuration: function (action, key, duration) {
             var actionKey = action;
             if (key.length > 0) actionKey += "-" + key;
             this.actionDurationEndTimestamps[actionKey] = new Date().getTime() + duration * 1000;
         },
-        
+
         getActionDuration: function (action, key) {
             var actionKey = action;
             if (key.length > 0) actionKey += "-" + key;
@@ -145,13 +157,13 @@ define(['ash', 'game/worldcreator/WorldCreatorHelper'], function (Ash, WorldCrea
             }
             return 0;
         },
-        
+
         getActionLocationKey: function (isLocationAction, playerPos) {
             var locationKey = "";
             if (isLocationAction) locationKey = playerPos.level + "-" + playerPos.sectorId();
             return locationKey;
         }
-        
+
     });
 
     return GameState;
