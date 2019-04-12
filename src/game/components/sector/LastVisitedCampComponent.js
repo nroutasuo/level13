291e658b54152f9521a384e5e2b62b380f892783
nroutasuo <noora.routasuo@gmail.com>
2019-04-12 19:50:55 +0200

    improve save not loaded popup

--- src/game/components/sector/LastVisitedCampComponent.js
+++ src/game/components/sector/LastVisitedCampComponent.js
@@ -1,9 +1,14 @@
 // Defines the given entity (sector) as the last one the player visited
 define(['ash'], function (Ash) {
     var LastVisitedCampComponent = Ash.Class.extend({
+        
         constructor: function () {
             this.isLastVisitedCamp = true;
-        }
+        },
+
+        getSaveKey: function () {
+            return "LastVisitedCampComponent";
+        },
     });
 
     return LastVisitedCampComponent;
