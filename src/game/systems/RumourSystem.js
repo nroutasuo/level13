4704aa0e0b3a815bbac34f79db0ab40501b05c45
nroutasuo <noora.routasuo@gmail.com>
2019-06-02 11:24:42 +0200

    add passive rumour generation to markets

--- src/game/systems/RumourSystem.js
+++ src/game/systems/RumourSystem.js
@@ -40,6 +40,7 @@ define([
 			rumoursComponent.accumulation = 0;
 			
 			var campfireUpgradeLevel = this.getImprovementUpgradeLevel(improvementNames.campfire);
+            var marketUpgradeLevel = this.getImprovementUpgradeLevel(improvementNames.market);
 			var innUpgradeLevel = this.getImprovementUpgradeLevel(improvementNames.inn);
 			
 			if (this.campNodes.head) {
@@ -48,6 +49,8 @@ define([
                 var improvementsComponent;
                 var campfireCount = 0;
 				var campfireFactor = 1;
+                var marketCount;
+                var marketFactor = 1;
                 var innCount = 0;
                 var innFactor = 1;
 				for (var campNode = this.campNodes.head; campNode; campNode = campNode.next) {
@@ -57,19 +60,25 @@ define([
 					campfireFactor = CampConstants.RUMOUR_BONUS_PER_CAMPFIRE_BASE;
 					campfireFactor += campfireUpgradeLevel > 1 ? (campfireUpgradeLevel - 1) * CampConstants.RUMOURS_BONUS_PER_CAMPFIRE_PER_UPGRADE : 0;
                     
+                    marketCount = improvementsComponent.getCount(improvementNames.market);
+                    marketFactor = CampConstants.RUMOUR_BONUS_PER_MARKET_BASE;
+					marketFactor += marketUpgradeLevel > 1 ? (marketUpgradeLevel - 1) * CampConstants.RUMOURS_BONUS_PER_MARKET_PER_UPGRADE : 0;
+                    
                     innCount = improvementsComponent.getCount(improvementNames.inn);
                     innFactor = CampConstants.RUMOUR_BONUS_PER_INN_BASE;
 					innFactor += innUpgradeLevel > 1 ? (innUpgradeLevel - 1) * CampConstants.RUMOURS_BONUS_PER_INN_PER_UPGRADE : 0;
                     
 					var accSpeedPopulation = CampConstants.RUMOURS_PER_POP_PER_SEC_BASE * Math.floor(campNode.camp.population) * GameConstants.gameSpeedCamp;
 					var accSpeedCampfire = campfireCount > 0 ? Math.pow(campfireFactor, campfireCount) * accSpeedPopulation - accSpeedPopulation : 0;
+                    var accSpeedMarket = marketCount > 0 ?  Math.pow(marketFactor, marketCount) * accSpeedPopulation - accSpeedPopulation : 0;
 					var accSpeedInn = innCount > 0 ? Math.pow(innFactor, innCount) * accSpeedPopulation - accSpeedPopulation : 0;
                     
-					var accSpeedCamp = accSpeedPopulation + accSpeedCampfire + accSpeedInn;
+					var accSpeedCamp = accSpeedPopulation + accSpeedCampfire + accSpeedMarket + accSpeedInn;
 					accSpeed += accSpeedCamp;
 					
 					rumoursComponent.addChange("Population", accSpeedPopulation);
 					rumoursComponent.addChange("Campfires", accSpeedCampfire);
+					if (marketFactor > 1) rumoursComponent.addChange("Markets", accSpeedMarket);
 					if (innFactor > 1) rumoursComponent.addChange("Inns", accSpeedInn);
 					rumoursComponent.accumulation += accSpeed;
 				}
