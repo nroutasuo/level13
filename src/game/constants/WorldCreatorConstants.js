12616149fbdd1fa9fb27eb5957c6f4a13194cb1d
nroutasuo <noora.routasuo@gmail.com>
2020-02-17 21:10:27 +0100

    move camp ordinal limit

--- src/game/constants/WorldCreatorConstants.js
+++ src/game/constants/WorldCreatorConstants.js
@@ -60,7 +60,7 @@ define(['ash', 'utils/MathUtils'], function (Ash, MathUtils) {
         FIRST_CAMP_X: 1,
         FIRST_CAMP_Y: 0,
         
-        CAMP_ORDINAL_LIMIT: 7,
+        CAMP_ORDINAL_LIMIT: 8,
         
         MIN_LEVEL_ORDINAL_HAZARD_RADIATION: 5,
         MIN_LEVEL_ORDINAL_HAZARD_POISON: 3,
