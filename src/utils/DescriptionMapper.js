0484ad26e4592bd4a1175114b21d99d81e94190c
nroutasuo <noora.routasuo@gmail.com>
2020-03-03 13:38:32 +0100

    [text] handle multiple matches in DescriptionMapper better (select pseudo-random)

--- src/utils/DescriptionMapper.js
+++ src/utils/DescriptionMapper.js
@@ -1,9 +1,10 @@
-// maps a number of properties to text descriptions, for example sector features to a sector description
+// maps a number of properties to text descriptions / templates, for example sector features to a sector description
 // add descriptions with properties required to match them
 // get description with an object
-// - returns description whose properties all match that of the object
+// - returns description whose properties ALL match that of the object
 // - if several match, returns most specific description (most properties)
-// properties may be simple (equals) or ranges (array of two values (inclusive))
+// - if several match and they are equally specific, returns a "random" one from matching (guaranteed to be the same one every time for the same properties)
+// properties in descriptions may be simple (equals) or ranges (array of two values (inclusive))
 
 define(function () {
     var DescriptionMapper = {
@@ -22,25 +23,38 @@ define(function () {
                 return "";
             }
             
-            var topMatch = null;
-            var topMatchNum = 0;
+            var bestMatches = [];
+            var bestMatchesScore = 0;
             for (var i = 0; i < this.descriptions[type].length; i++) {
                 var desc = this.descriptions[type][i];
                 if (this.matches(props, desc)) {
-                    var num = Object.keys(desc.props).length;
-                    if (!topMatch || topMatchNum < num) {
-                        topMatch = desc;
-                        topMatchNum = num;
+                    var score = this.getMatchScore(desc);
+                    if (bestMatchesScore == score) {
+                        // same score, add to list
+                        bestMatches.push(desc);
+                    } else if (bestMatchesScore < score) {
+                        // higher score, reset list
+                        bestMatches = [desc];
+                        bestMatchesScore = score;
                     }
                 }
             }
             
-            if (!topMatch) {
+            // no matches: warning
+            if (bestMatches.length == 0) {
                 log.w("no description found with type " + type);
                 return "";
             }
             
-            return topMatch.text;
+            // single match, return that
+            if (bestMatches.length == 1) {
+                return bestMatches[0].text;
+            }
+            
+            // several matches, select one in a semi-random way (same object should return the same one if called again but should be different for different objects)
+            var checksum = this.getPropsChecksum(props);
+            var index = checksum % bestMatches.length;
+            return bestMatches[index].text;
         },
         
         matches: function (props, desc) {
@@ -57,6 +71,23 @@ define(function () {
             }
             return true;
         },
+        
+        getMatchScore: function (desc) {
+             return Object.keys(desc.props).length;
+        },
+        
+        getPropsChecksum: function (props) {
+            var result = 0;
+            for (var [key, value] of Object.entries(props)) {
+                var t = typeof value;
+                if (t == "number") {
+                    result += value;
+                } else {
+                    if (value) result++;
+                }
+            }
+            return Math.round(result);
+        }
         
     };
     return DescriptionMapper;
