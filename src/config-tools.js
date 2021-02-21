bf4b4749e3ce0b1269c4e1c13559e5c625828a54
nroutasuo <noora.routasuo@gmail.com>
2021-02-21 17:15:23 +0100

    [qa] add tools page and tool to fix previous saves for the compass/knife blocker

---
+++ src/config-tools.js
@@ -0,0 +1,14 @@
+require.config({
+
+    deps: ["player-tools"],
+
+    baseUrl: 'src',
+
+    paths: {
+        ash: "../lib/ash/ash.min",
+        jquery: "../lib/jquery/",
+        lzstring: "../lib/lzstring",
+        game: "game"
+    },
+
+});
