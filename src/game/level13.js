a0c3d15ac9d7283f371f375768f0790d1b61524b
nroutasuo <noora.routasuo@gmail.com>
2021-03-02 23:31:35 +0100

    improve exception popup auto-generated Github issue details

--- src/game/level13.js
+++ src/game/level13.js
@@ -309,10 +309,11 @@ define([
 			GameGlobals.uiFunctions.hideGame(false);
 			
 			// show popup
+			var pos = GameGlobals.playerActionFunctions.playerPositionNodes.head ? GameGlobals.playerActionFunctions.playerPositionNodes.head.position : "(unknown)";
 			var bugTitle = StringUtils.encodeURI("[JS Error] " + desc.title);
 			var bugBody = StringUtils.encodeURI(
 			   "Details:\n[Fill in any details here that you think will help tracking down this bug, such as what you did in the game just before it happened.]" +
-			   "\n\nStacktrace:\n" + desc.stack);
+			   "\n\nSeed: " + GameGlobals.gameState.worldSeed + "\nPosition: " + pos + "\nStacktrace:\n" + desc.stack);
 			var url = "https://github.com/nroutasuo/level13/issues/new?title=" + bugTitle + "&body=" + bugBody + "&labels=exception";
 			GameGlobals.uiFunctions.popupManager.closeAllPopups();
 			GameGlobals.uiFunctions.showInfoPopup(
