d3628234500caea156469ade210ef8037ee232d5
nroutasuo <noora.routasuo@gmail.com>
2022-08-28 17:57:54 +0200

    [ui] fix camp showing population decreasing when it's not

--- src/game/systems/ui/UIOutCampSystem.js
+++ src/game/systems/ui/UIOutCampSystem.js
@@ -204,7 +204,7 @@
 		updatePopulationDisplay: function (campComponent, maxPopulation, reputation, robots, maxRobots) {
 			var freePopulation = campComponent.getFreePopulation();
 			var isPopulationMaxed = campComponent.population >= maxPopulation;
-			var populationChangePerSec = campComponent.populationChangePerSec;
+			var populationChangePerSec = campComponent.populationChangePerSec || 0;
 			var isPopulationStill = isPopulationMaxed && populationChangePerSec === 0;
 
 			var reqRepCur = CampConstants.getRequiredReputation(Math.floor(campComponent.population));
