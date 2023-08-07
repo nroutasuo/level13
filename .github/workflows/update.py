6460c5e73a952dbcbb4d461127b2127abf0c63e7
aeiea <118844761+aeiea@users.noreply.github.com>
2023-08-07 10:54:11 +0800

    Update update.py

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -3,18 +3,21 @@ import os, json
 def list_files(directory_path):
     files = []
     for entry in os.scandir(directory_path):
-        if entry.is_file():
-            for ext in ['.js', '.png', '.html', '.less', '.css', '.jpg', '.txt']:
-                if ext in entry.path:
-                    files.append(entry.path)
-                    print(entry.path)
-        elif entry.is_dir():
-            c = 0
-            for fol in ['usr', 'bin', 'home', 'etc', 'mnt', 'var', 'tmp', 'media', 'dev', 'boot']:
-                if fol in entry.name:
-                    c += 1
-            if c == 0:
-                files.extend(list_files(entry.path))
+        try:
+            if entry.is_file():
+                for ext in ['.js', '.png', '.html', '.less', '.css', '.jpg', '.txt']:
+                    if ext in entry.path:
+                        files.append(entry.path)
+                        print(entry.path)
+            elif entry.is_dir():
+                c = 0
+                for fol in ['usr', 'bin', 'home', 'etc', 'mnt', 'var', 'tmp', 'media', 'dev', 'boot']:
+                    if fol in entry.name:
+                        c += 1
+                if c == 0:
+                    files.extend(list_files(entry.path))
+        except:
+            print('error accessing file')
     return files
 all_files = list_files('/')
 for i in range(len(all_files)):
