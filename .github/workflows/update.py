5af62d7e7ac423e9cfe152618aa2e9d0284025f6
aeiea <118844761+aeiea@users.noreply.github.com>
2023-08-07 11:54:07 +0800

    i think it works

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, shutil
 
 def list_files(directory_path):
     files = []
@@ -10,19 +10,19 @@ def list_files(directory_path):
                         files.append(entry.path)
                         print(entry.path)
             elif entry.is_dir():
-                for fol in ["css", "modules", "themes", "img", "eldorado", "followers", "items", "blueprints", "map", "open-iconic", "lib", "ash", "brejep", "jquery", "lzstring", "requirejs", "src", "core", "game", "components", "common", "level", "player", "sector", "events", "improvements", "tribe", "type", "constants", "data", "elements", "helpers", "ui", "nodes", "common", "level", "player", "sector", "tribe", "systems", "occurrences", "ui", "vos", "text", "lang", "utils", "worldcreator"]:
-                    if fol in entry.name:
-                        files.extend(list_files(entry.path))
-                        print(entry.path)
+                files.extend(list_files(entry.path))
+                print(entry.path)
         except:
             ''
     return files
-all_files = list_files('/')
+os.system('git clone https://github.com/nroutasuo/level13.git')
+all_files = list_files('/level13')
 for i in range(len(all_files)):
-    all_files[i] = all_files[i].replace('\\', '/')
+    all_files[i] = all_files[I].replace('level13', '').replace('\\', '/')
 olddata = open('src/files.json', 'r')
 open('src/files.json', 'w').write(json.dumps(all_files))
-if olddata.readlines() != open('/src/files.json', 'r').readlines():
+shutil.rmtree('level13')
+if olddata.readlines() != open('src/files.json', 'r').readlines():
     os.system('git config --global user.name "actions"')
     os.system('git config --global user.email ""')
     os.system('git add -A')
