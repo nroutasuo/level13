719c8cdcdaf922c5713952e7bf45dc3fe4d6157e
nroutasuo <noora.routasuo@gmail.com>
2025-01-29 22:14:30 +0100

    [dialogue] simplify parsing of multi-step dialogues

--- src/game/constants/DialogueConstants.js
+++ src/game/constants/DialogueConstants.js
@@ -1,194 +1,195 @@
 define([
-    'ash', 
-    'json!game/data/DialogueData.json', 
-    'game/vos/DialogueVO', 
-    'game/vos/DialoguePageVO', 
-    'game/vos/DialogueOptionVO', 
-    'game/vos/ResultVO'
+	'ash', 
+	'json!game/data/DialogueData.json', 
+	'game/vos/DialogueVO', 
+	'game/vos/DialoguePageVO', 
+	'game/vos/DialogueOptionVO', 
+	'game/vos/ResultVO'
 ], function (Ash, DialogueData, DialogueVO, DialoguePageVO, DialogueOptionVO, ResultVO) {
 	
 	let DialogueConstants = {
 
-        dialogueSettings: {
-            meet: "meet", // when meeting outside
-            event: "event", // when event relevant to the NPC is active (recruit, visit)
-            interact: "interact", // when player chooses to interact with this NPC
-        },
+		dialogueSettings: {
+			meet: "meet", // when meeting outside
+			event: "event", // when event relevant to the NPC is active (recruit, visit)
+			interact: "interact", // when player chooses to interact with this NPC
+		},
 
-        STATUS_NEW: 1,
-        STATUS_URGENT: 2,
-        STATUS_FORCED: 3,
+		STATUS_NEW: 1,
+		STATUS_URGENT: 2,
+		STATUS_FORCED: 3,
 
 		dialogueSources: {},
 
-        dialogues: {},
+		dialogues: {},
 
-        init: function () {
-        },
+		init: function () {
+		},
 
-        loadData: function (data) {
-            let sourcesRaw = data.sources;
-            this.dialogueSources = {};
+		loadData: function (data) {
+			let sourcesRaw = data.sources;
+			this.dialogueSources = {};
 
-            for (let dialogueSourceID in sourcesRaw) {
-                let source = sourcesRaw[dialogueSourceID];
-                source.id = dialogueSourceID;
-                this.dialogueSources[dialogueSourceID] = source;
-            }
+			for (let dialogueSourceID in sourcesRaw) {
+				let source = sourcesRaw[dialogueSourceID];
+				source.id = dialogueSourceID;
+				this.dialogueSources[dialogueSourceID] = source;
+			}
 
-            let dialoguesRaw = data.dialogues;
-            let dialogues = {};
+			let dialoguesRaw = data.dialogues;
+			let dialogues = {};
 
-            for (let dialogueID in dialoguesRaw) {
-                let d = dialoguesRaw[dialogueID];
-                let vo = this.parseDialogue(dialogueID, d);
+			for (let dialogueID in dialoguesRaw) {
+				let d = dialoguesRaw[dialogueID];
+				let vo = this.parseDialogue(dialogueID, d);
 
-                dialogues[dialogueID] = vo;
-            }
+				dialogues[dialogueID] = vo;
+			}
 
-            this.dialogues = dialogues;
-        },
+			this.dialogues = dialogues;
+		},
 
-        parseDialogue: function (dialogueID, d) {
-            let vo = new DialogueVO(dialogueID);
+		parseDialogue: function (dialogueID, d) {
+			let vo = new DialogueVO(dialogueID);
 
-            vo.conditions = d.conditions || {};
-            vo.storyTag = d.storyTag || null;
-            vo.isRepeatable = d.repeatable === false ? false : true;
-            vo.isUrgent = d.urgent;
+			vo.conditions = d.conditions || {};
+			vo.storyTag = d.storyTag || null;
+			vo.isRepeatable = d.repeatable === false ? false : true;
+			vo.isUrgent = d.urgent;
 
-            for (let i = 0; i < d.pages.length; i++) {
-                let pageData = d.pages[i];
-                let pageVO = this.parsePage(i, pageData);
+			for (let i = 0; i < d.pages.length; i++) {
+				let pageData = d.pages[i];
+				let pageVO = this.parsePage(i, pageData);
 
-                vo.pages.push(pageVO);
-                vo.pagesByID[pageVO.pageID] = pageVO;
-            }
+				vo.pages.push(pageVO);
+				vo.pagesByID[pageVO.pageID] = pageVO;
+			}
 
-            for (let i = 0; i < vo.pages.length; i++) {
-                for (let j = 0; j < vo.pages[i].options.length; j++) {
-                    let optionVO = vo.pages[i].options[j];
-                    if (optionVO.responsePageID === "NEXT") {
-                        optionVO.responsePageID = vo.pages[i+1].pageID;
-                    } 
-                }
-            }
+			for (let i = 0; i < vo.pages.length; i++) {
+				for (let j = 0; j < vo.pages[i].options.length; j++) {
+					let optionVO = vo.pages[i].options[j];
+					if (optionVO.responsePageID === "NEXT") {
+						optionVO.responsePageID = vo.pages[i+1].pageID;
+					} 
+				}
+			}
 
-            return vo;
-        },
+			return vo;
+		},
 
-        parsePage: function (i, pageData) {
-            let pageID = pageData.id || i;
-            let pageVO = new DialoguePageVO(pageID);
-            let pageKey = typeof pageData === "string" ? pageData : pageData.key;
-            pageVO.textKey = "story.dialogue." + pageKey;
-            
-            pageVO.options = [];
+		parsePage: function (i, pageData) {
+			let pageID = pageData.id || i;
+			let pageVO = new DialoguePageVO(pageID);
+			let pageKey = typeof pageData === "string" ? pageData : pageData.key;
+			pageVO.textKey = "story.dialogue." + pageKey;
+			
+			pageVO.options = [];
 
-            if (pageData.options === "NEXT") {
-                pageData.options = [ "NEXT" ];
-            }
+			if (pageData.options === "NEXT") {
+				let buttonKey = pageData.buttonKey;
+				pageData.options = [ { buttonKey: buttonKey, response: "NEXT" } ];
+			}
 
-            for (let j in pageData.options) {
-                let optionData = pageData.options[j];
+			for (let j in pageData.options) {
+				let optionData = pageData.options[j];
 
-                let optionVO = this.parseDialogueOption(j, optionData);
+				let optionVO = this.parseDialogueOption(j, optionData);
 
-                pageVO.options.push(optionVO);
-                pageVO.optionsByID[optionVO.optionID] = optionVO;
-            }
+				pageVO.options.push(optionVO);
+				pageVO.optionsByID[optionVO.optionID] = optionVO;
+			}
 
-            pageVO.resultTemplate = this.parsePageResult(pageData.result);
+			pageVO.resultTemplate = this.parsePageResult(pageData.result);
 
-            return pageVO;
-        },
+			return pageVO;
+		},
 
-        parsePageResult: function (data) {
-            if (!data) return null;
+		parsePageResult: function (data) {
+			if (!data) return null;
 
-            let resultVO = new ResultVO("start_dialogue_page");
+			let resultVO = new ResultVO("start_dialogue_page");
 
-            if (data.gainedRumours) resultVO.gainedRumours = parseInt(data.gainedRumours);
+			if (data.gainedRumours) resultVO.gainedRumours = parseInt(data.gainedRumours);
 
-            if (data.gainedPopulation) resultVO.gainedPopulation = parseInt(data.gainedPopulation);
+			if (data.gainedPopulation) resultVO.gainedPopulation = parseInt(data.gainedPopulation);
 
-            if (data.gainedItems) {
-                resultVO.gainedItems = [];
-                for (let i in data.gainedItems) {
-                    // TODO check item exists
-                    let itemID = data.gainedItems[i];
-                    resultVO.gainedItems.push(itemID);
-                }
-            }
+			if (data.gainedItems) {
+				resultVO.gainedItems = [];
+				for (let i in data.gainedItems) {
+					// TODO check item exists
+					let itemID = data.gainedItems[i];
+					resultVO.gainedItems.push(itemID);
+				}
+			}
 
-            if (data.gainedPerks) {
-                resultVO.gainedPerks = [];
-                for (let i in data.gainedPerks) {
-                    // TODO check perk exists (or is general injury)
-                    let perkID = data.gainedPerks[i];
-                    resultVO.gainedPerks.push(perkID);
-                }
-            }
+			if (data.gainedPerks) {
+				resultVO.gainedPerks = [];
+				for (let i in data.gainedPerks) {
+					// TODO check perk exists (or is general injury)
+					let perkID = data.gainedPerks[i];
+					resultVO.gainedPerks.push(perkID);
+				}
+			}
 
-            if (data.gainedItemUpgrades) {
-                resultVO.gainedItemUpgrades = data.gainedItemUpgrades;
-            }
+			if (data.gainedItemUpgrades) {
+				resultVO.gainedItemUpgrades = data.gainedItemUpgrades;
+			}
 
-            if (data.storyFlags) {
-                for (let flagID in data.storyFlags) {
-                    resultVO.storyFlags[flagID] = data.storyFlags[flagID];
-                }
-            }
+			if (data.storyFlags) {
+				for (let flagID in data.storyFlags) {
+					resultVO.storyFlags[flagID] = data.storyFlags[flagID];
+				}
+			}
 
-            return resultVO;
-        },
+			return resultVO;
+		},
 
-        parseDialogueOption: function (i, optionData) {
-            let optionID = i;
-            let optionVO = new DialogueOptionVO(optionID);
+		parseDialogueOption: function (i, optionData) {
+			let optionID = i;
+			let optionVO = new DialogueOptionVO(optionID);
 
-            if (typeof optionData === "string") {
-                optionVO.responsePageID = optionData;
-            } else {
-                optionVO.buttonTextKey = optionData.buttonKey || null;
-                optionVO.costs = optionData.costs || {};
-                optionVO.responsePageID = optionData.response || null;
-            }
+			if (typeof optionData === "string") {
+				optionVO.responsePageID = optionData;
+			} else {
+				optionVO.buttonTextKey = optionData.buttonKey || null;
+				optionVO.costs = optionData.costs || {};
+				optionVO.responsePageID = optionData.response || null;
+			}
 
-            return optionVO;
-        },
+			return optionVO;
+		},
 
-        getDialogueEntries: function (sourceID, setting) {
-            let source = this.getDialogueSource(sourceID);
-            if (!source || !source.dialogues) {
-                log.w("no valid dialogue entries found: " + sourceID);
-                return [];
-            }
-            return source.dialogues[setting] || [];
-        },
+		getDialogueEntries: function (sourceID, setting) {
+			let source = this.getDialogueSource(sourceID);
+			if (!source || !source.dialogues) {
+				log.w("no valid dialogue entries found: " + sourceID);
+				return [];
+			}
+			return source.dialogues[setting] || [];
+		},
 
-        getDialogue: function (dialogueID) {
-            let result = this.dialogues[dialogueID];
-            if (!result) {
-                log.w("no such dialogue found: " + dialogueID);
-                return {};
-            }
-            return result;
-        },
+		getDialogue: function (dialogueID) {
+			let result = this.dialogues[dialogueID];
+			if (!result) {
+				log.w("no such dialogue found: " + dialogueID);
+				return {};
+			}
+			return result;
+		},
 
-        getDialogueSource: function (sourceID) {
-            let result = this.dialogueSources[sourceID]
-            if (!result) {
-                log.w("no such dialogue source found: " + sourceID);
-                return {};
-            }
-            return result;
-        },
+		getDialogueSource: function (sourceID) {
+			let result = this.dialogueSources[sourceID]
+			if (!result) {
+				log.w("no such dialogue source found: " + sourceID);
+				return {};
+			}
+			return result;
+		},
 		
 	};
 
-    DialogueConstants.init();
-    DialogueConstants.loadData(DialogueData);
+	DialogueConstants.init();
+	DialogueConstants.loadData(DialogueData);
 	
 	return DialogueConstants;
 	
