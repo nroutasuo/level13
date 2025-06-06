aff80645694e03a35287d993eb0eed9324c1cbef
nroutasuo <noora.routasuo@gmail.com>
2025-06-06 16:27:53 +0200

    [text] add dedicated dialogues for explorers when outside

--- src/game/helpers/DialogueHelper.js
+++ src/game/helpers/DialogueHelper.js
@@ -146,6 +146,7 @@ define(['ash',
                     if (conditions.explorer.trust && conditions.explorer.trust > explorerVO.trust) return false;
                     if (conditions.explorer.maxTrust && conditions.explorer.maxTrust < explorerVO.trust) return false;
                     if (conditions.explorer.injured && explorerVO.injuredTimer <= 0) return false;
+                    if (conditions.explorer.abilityType && explorerVO.abilityType != conditions.explorer.abilityType) return false;
                     if (conditions.explorer.quest && GameGlobals.storyHelper.getExplorerQuestStories(explorerVO).indexOf(conditions.explorer.quest) <0) return false;
                     if (conditions.explorer.meetCampOrdinal 
                         && GameGlobals.playerActionsHelper.checkRequirementsRange(conditions.explorer.meetCampOrdinal, explorerVO.meetCampOrdinal)) return false;
