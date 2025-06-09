5160f22eae35ef461cbddf08d6da14d21cfe4c14
nroutasuo <noora.routasuo@gmail.com>
2025-06-09 22:33:41 +0200

    [balancing] make disease just a little bit less frequent

--- src/game/constants/OccurrenceConstants.js
+++ src/game/constants/OccurrenceConstants.js
@@ -32,7 +32,7 @@ define(['ash', 'utils/MathUtils', 'game/constants/CampConstants', 'game/constant
 		campOccurrenceCooldowns: {
 			accident: 60 * 90,
 			disaster: 60 * 50,
-			disease: 60 * 15,
+			disease: 60 * 20,
 			raid: 60 * 20,
 			recruit: 60 * 25,
 			refugees: 60 * 120,
