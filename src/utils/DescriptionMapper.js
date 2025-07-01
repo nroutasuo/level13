4fd855ae14997b1ba72391d76f092b3b7769648b
nroutasuo <noora.routasuo@gmail.com>
2025-07-01 20:09:12 +0200

    [qa] fix a crash when entering certain sectors

--- src/utils/DescriptionMapper.js
+++ src/utils/DescriptionMapper.js
@@ -81,7 +81,7 @@ define(function () {
 			for (var [key, value] of Object.entries(props)) {
 				let t = typeof value;
 				if (t == "number") {
-					result += value;
+					result += Math.abs(value);
 				} else {
 					if (value) result++;
 				}
